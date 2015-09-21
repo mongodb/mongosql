7393d8a059c66d60f438b7179535d3897eae3975
Wisdom Omuya <deafgoat@gmail.com>
2015-09-21 16:51:26 -0400

    comparator test skeleton

---
+++ translator/comparator/compare_test.go
@@ -0,0 +1,17 @@
+package comparator
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestCompare(t *testing.T) {
+
+	Convey("When calling Compare...", t, func() {
+
+		Convey("", func() {
+
+		})
+
+	})
+}
