44c3a5888527722cde6233e8119ba1b5751eb55d
Wisdom Omuya <deafgoat@gmail.com>
2015-10-14 11:00:30 -0400

    remove panic on non-numeric fields

--- evaluator/binary_functions.go
+++ evaluator/binary_functions.go
@@ -38,98 +38,53 @@ func (sqlfunc *SQLBinaryExprValue) MongoValue() interface{} {
 
 var binaryFuncMap = map[string]SQLBinaryFunction{
 	"+": SQLBinaryFunction(func(args []SQLValue, ctx *EvalCtx) (SQLValue, error) {
-		var sum SQLNumeric
-		sum = SQLInt(0)
-		for _, arg := range args {
-			c, err := arg.Evaluate(ctx)
-			if err != nil {
-				return nil, err
-			}
-			switch v := c.(type) {
-			case SQLNumeric:
-				sum = sum.Add(v)
-			case SQLNullValue:
-				// treat null as 0
-				continue
-			default:
-				return nil, fmt.Errorf("illegal argument: not numeric")
-			}
-		}
-		return sum, nil
+		return SQLNumericBinaryOp(args, ctx, "+")
+
 	}),
 	"-": SQLBinaryFunction(func(args []SQLValue, ctx *EvalCtx) (SQLValue, error) {
-		if len(args) < 2 {
-			return nil, fmt.Errorf("- function needs at least 2 args")
-		}
-		var diff *SQLNumeric
-		for _, arg := range args {
-			c, err := arg.Evaluate(ctx)
-			if err != nil {
-				return nil, err
-			}
-			switch v := c.(type) {
-			case SQLNumeric:
-				if diff == nil {
-					diff = &v
-				} else {
-					d := (*diff).Sub(v)
-					diff = &d
-				}
-			case SQLNullValue:
-				// treat null as 0
-				continue
-			default:
-				return nil, fmt.Errorf("illegal argument: not numeric")
-			}
-		}
-		return *diff, nil
+		return SQLNumericBinaryOp(args, ctx, "-")
 	}),
 	"*": SQLBinaryFunction(func(args []SQLValue, ctx *EvalCtx) (SQLValue, error) {
-		if len(args) < 2 {
-			return nil, fmt.Errorf("* function needs at least 2 args")
-		}
-		var product SQLNumeric = SQLFloat(1)
-		for _, arg := range args {
-			c, err := arg.Evaluate(ctx)
-			if err != nil {
-				return nil, err
-			}
-			switch v := c.(type) {
-			case SQLNumeric:
-				product = product.Product(v)
-			case SQLNullValue:
-				// treat null as 0
-				continue
-			default:
-				return nil, fmt.Errorf("illegal argument: not numeric")
-			}
-		}
-		return SQLNumeric(product), nil
+		return SQLNumericBinaryOp(args, ctx, "*")
 	}),
 	"/": SQLBinaryFunction(func(args []SQLValue, ctx *EvalCtx) (SQLValue, error) {
-		if len(args) != 2 {
-			return nil, fmt.Errorf("/ function must take 2 args")
-		}
+		return SQLNumericBinaryOp(args, ctx, "/")
+	}),
+}
 
-		lEval, err := args[0].Evaluate(ctx)
-		if err != nil {
-			return nil, err
-		}
-		left, leftOk := (lEval).(SQLNumeric)
+func SQLNumericBinaryOp(args []SQLValue, ctx *EvalCtx, op string) (SQLValue, error) {
+	if len(args) < 2 {
+		return nil, fmt.Errorf("%v function needs at least 2 args", op)
+	}
 
-		rEval, err := args[1].Evaluate(ctx)
-		if err != nil {
-			return nil, err
-		}
-		right, rightOk := (rEval).(SQLNumeric)
+	lEval, err := args[0].Evaluate(ctx)
+	if err != nil {
+		return nil, err
+	}
+	left, leftOk := (lEval).(SQLNumeric)
 
-		if !(leftOk && rightOk) {
-			return nil, fmt.Errorf("both args to / must be numeric")
-		}
+	rEval, err := args[1].Evaluate(ctx)
+	if err != nil {
+		return nil, err
+	}
+	right, rightOk := (rEval).(SQLNumeric)
 
+	if !(leftOk && rightOk) {
+		return &SQLNullValue{}, nil
+	}
+
+	switch op {
+	case "+":
+		return left.Add(right), nil
+	case "-":
+		return left.Sub(right), nil
+	case "*":
+		return left.Product(right), nil
+	case "/":
 		if right.Float64() == 0 {
-			return nil, fmt.Errorf("divide by zero")
+			return &SQLNullValue{}, nil
 		}
 		return SQLFloat(left.Float64() / right.Float64()), nil
-	}),
+	}
+	return nil, fmt.Errorf("unsupported numeric binary operation: '%v'", op)
 }
