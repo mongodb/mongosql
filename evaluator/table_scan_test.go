3efb5d06150ddd8d2b6f6be72fee502ada32db4e
Michael O'Brien <michaelobrien@Michaels-MacBook-Pro.local>
2015-10-29 15:57:26 -0400

    add tests for extractfieldbyname

--- evaluator/table_scan_test.go
+++ evaluator/table_scan_test.go
@@ -74,3 +74,68 @@ func TestTableScanOperator(t *testing.T) {
 		})
 	})
 }
+
+func TestExtractField(t *testing.T) {
+	Convey("With a test bson.D", t, func() {
+		testD := bson.D{
+			{"a", "string"},
+			{"b", []interface{}{"inner", bson.D{{"inner2", 1}}}},
+			{"c", bson.D{{"x", 5}}},
+			{"d", bson.D{{"z", nil}}},
+		}
+
+		Convey("regular fields should be extracted by name", func() {
+			val := extractFieldByName("a", testD)
+			So(val, ShouldEqual, "string")
+		})
+
+		Convey("array fields should be extracted by name", func() {
+			val := extractFieldByName("b.1", testD)
+			So(val, ShouldResemble, bson.D{{"inner2", 1}})
+			val = extractFieldByName("b.1.inner2", testD)
+			So(val, ShouldEqual, 1)
+			val = extractFieldByName("b.0", testD)
+			So(val, ShouldEqual, "inner")
+		})
+
+		Convey("subdocument fields should be extracted by name", func() {
+			val := extractFieldByName("c", testD)
+			So(val, ShouldResemble, bson.D{{"x", 5}})
+			val = extractFieldByName("c.x", testD)
+			So(val, ShouldEqual, 5)
+
+			Convey("even if they contain null values", func() {
+				val := extractFieldByName("d", testD)
+				So(val, ShouldResemble, bson.D{{"z", nil}})
+				val = extractFieldByName("d.z", testD)
+				So(val, ShouldEqual, nil)
+				val = extractFieldByName("d.z.nope", testD)
+				So(val, ShouldEqual, "")
+			})
+		})
+
+		Convey(`non-existing fields should return ""`, func() {
+			val := extractFieldByName("f", testD)
+			So(val, ShouldEqual, "")
+			val = extractFieldByName("c.nope", testD)
+			So(val, ShouldEqual, "")
+			val = extractFieldByName("c.nope.NOPE", testD)
+			So(val, ShouldEqual, "")
+			val = extractFieldByName("b.1000", testD)
+			So(val, ShouldEqual, "")
+			val = extractFieldByName("b.1.nada", testD)
+			So(val, ShouldEqual, "")
+		})
+
+	})
+
+	Convey(`Extraction of a non-document should return ""`, t, func() {
+		val := extractFieldByName("meh", []interface{}{"meh"})
+		So(val, ShouldEqual, "")
+	})
+
+	Convey(`Extraction of a nil document should return ""`, t, func() {
+		val := extractFieldByName("a", nil)
+		So(val, ShouldEqual, "")
+	})
+}
