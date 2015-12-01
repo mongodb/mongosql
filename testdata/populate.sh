1fe88e12829cd006bba40f2e9a4fe16aff3e1678
Wisdom Omuya <deafgoat@gmail.com>
2015-12-01 14:33:23 -0500

    add flight date column

--- testdata/populate.sh
+++ testdata/populate.sh
@@ -8,11 +8,11 @@
 # The real MySQL server can then be used as a reference point for checking the behavior of the SQLProxy server.
 
 set -v
-mongoexport --host bi-beta.mongodb.com --type=csv --fields="carrier_code,tail_num,flight_number,origin_airport_id,origin_city_market_id,origin_airport_code,origin_city_name,origin_state,dest_airport_id,dest_city_market_id,dest_airport_code,dest_city_name,dest_state,dest_time,diff_from_dep_time,dep_delay,arr_time,diff_from_arr_time,airline,cancelled,arr_delay,carrier_delay" -d tableau -c flights201406 > flights.csv;
+mongoexport --host bi-beta.mongodb.com --type=csv --fields="_id,flight_date,carrier_code,tail_num,flight_number,origin_airport_id,origin_city_market_id,origin_airport_code,origin_city_name,origin_state,dest_airport_id,dest_city_market_id,dest_airport_code,dest_city_name,dest_state,dep_time,diff_from_dep_time,dep_delay,arr_time,diff_from_arr_time,flight_time,airline,cancelled,arr_delay,carrier_delay" -d tableau -c flights201406 > flights.csv;
 mongoexport --host bi-beta.mongodb.com --type=csv --fields="loc.coordinates.0,loc.coordinates.1,airport_city,airport_code,airport_id,city,city_market_description,city_market_id,country,state,zip" -d tableau -c attendees > attendees.csv;
 mysql -u root -e 'DROP TABLE IF EXISTS flights' test
 mysql -u root -e 'DROP TABLE IF EXISTS attendees' test
-mysql -u root -e 'CREATE TABLE flights  (carrier_code varchar(255), tail_num varchar(255), flight_number int, origin_airport_id int, origin_city_market_id int, origin_airport_code varchar(255), origin_city_name varchar(255), origin_state varchar(255), dest_airport_id int, dest_city_market_id int, dest_airport_code varchar(255), dest_city_name varchar(255), dest_state varchar(255), dest_time int, diff_from_dep_time int, dep_delay int, arr_time int, diff_from_arr_time int, airline varchar(255), cancelled int, arr_delay int, carrier_delay int);' test
+mysql -u root -e 'CREATE TABLE flights  (carrier_code varchar(255), flight_date date, tail_num varchar(255), flight_number int, origin_airport_id int, origin_city_market_id int, origin_airport_code varchar(255), origin_city_name varchar(255), origin_state varchar(255), dest_airport_id int, dest_city_market_id int, dest_airport_code varchar(255), dest_city_name varchar(255), dest_state varchar(255), dest_time int, diff_from_dep_time int, dep_delay int, arr_time int, diff_from_arr_time int, airline varchar(255), cancelled int, arr_delay int, carrier_delay int);' test
 
 mysql -u root -e 'CREATE TABLE attendees (latitude double, longitude double, airport_city varchar(255),airport_code varchar(255),airport_id int,city varchar(255),city_market_description varchar(255),city_market_id int,country varchar(255),state varchar(255),zip varchar(255))' test
 CWD=`pwd`
