fec4e8f5d27e5af83bba7cc73938443db834867f
Craig Wilson <craiggwilson@gmail.com>
2016-05-12 11:51:51 -0400

    simple queries are transformed properly.

--- evaluator/new_algebrizer.go
+++ evaluator/new_algebrizer.go
@@ -9,54 +9,130 @@ import (
 	"github.com/kr/pretty"
 )
 
-// AlgebrizerContext holds information related to algebrization.
-type AlgebrizerContext struct {
-	dbName string
-	schema *schema.Schema
-}
+// Algebrize takes a parsed SQL statement and returns an algebrized form of the query.
+func Algebrize(selectStatement sqlparser.SelectStatement, dbName string, schema *schema.Schema) (PlanStage, error) {
 
-// NewAlgebrizerContext creates a new algebrizer context
-func NewAlgebrizerContext(dbName string, schema *schema.Schema) *AlgebrizerContext {
-	return &AlgebrizerContext{
+	fmt.Printf("\nSelect Statement: %# v", pretty.Formatter(selectStatement))
+
+	algebrizer := &algebrizer{
 		dbName: dbName,
 		schema: schema,
 	}
+
+	return algebrizer.translateSelectStatement(selectStatement)
 }
 
-// Algebrize takes a parsed SQL statement and returns an algebrized form of the query.
-func Algebrize(selectStatement sqlparser.SelectStatement, ctx *AlgebrizerContext) (PlanStage, error) {
+type algebrizer struct {
+	dbName  string
+	schema  *schema.Schema
+	columns []*Column
+}
 
-	fmt.Printf("\n%# v", pretty.Formatter(selectStatement))
+func (a *algebrizer) registerColumns(columns []*Column) {
+	a.columns = append(a.columns, columns...)
+}
 
+func (a *algebrizer) lookupColumn(tableName, columnName string) (*Column, error) {
+	var found *Column
+	for _, column := range a.columns {
+		if strings.EqualFold(column.Name, columnName) && (tableName == "" || strings.EqualFold(column.Table, tableName)) {
+			if found != nil {
+				fullColumnName := columnName
+				if tableName != "" {
+					fullColumnName = tableName + "." + columnName
+				}
+				return nil, fmt.Errorf("column %q in the field list is ambiguous", fullColumnName)
+			}
+			found = column
+		}
+	}
+
+	return found, nil
+}
+
+func (a *algebrizer) translateSelectStatement(selectStatement sqlparser.SelectStatement) (PlanStage, error) {
 	switch typedS := selectStatement.(type) {
 
 	case *sqlparser.Select:
-		return newAlgebrizeSelect(typedS, ctx)
+		return a.translateSelect(typedS)
 	default:
 		return nil, fmt.Errorf("can't handle select statement of type %T", selectStatement)
 	}
 }
 
-func newAlgebrizeSelect(sel *sqlparser.Select, ctx *AlgebrizerContext) (PlanStage, error) {
+func (a *algebrizer) translateSelect(sel *sqlparser.Select) (PlanStage, error) {
 	var plan PlanStage
 	var err error
 	if sel.From != nil {
-		plan, err = newAlgebrizeTableExprs(sel.From, ctx)
+		plan, err = a.translateTableExprs(sel.From)
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	fmt.Printf("\n%# v", pretty.Formatter(plan))
+	if sel.SelectExprs != nil {
+		sExprs, err := a.translateSelectExprs(sel.SelectExprs)
+		if err != nil {
+			return nil, err
+		}
+
+		plan = &ProjectStage{
+			source: plan,
+			sExprs: sExprs,
+		}
+	}
 
 	return plan, nil
 }
 
-func newAlgebrizeTableExprs(tableExprs sqlparser.TableExprs, ctx *AlgebrizerContext) (PlanStage, error) {
+func (a *algebrizer) translateSelectExprs(selectExprs sqlparser.SelectExprs) (SelectExpressions, error) {
+	var results SelectExpressions
+	for _, selectExpr := range selectExprs {
+		switch typedE := selectExpr.(type) {
+
+		// TODO: validate no mixture of star and non-star expression
+		case *sqlparser.StarExpr:
+
+			// validate tableName if present. Need to have a map of alias -> tableName -> schema
+
+		case *sqlparser.NonStarExpr:
+
+			translatedExpr, err := a.translateExpr(typedE.Expr)
+			if err != nil {
+				return nil, err
+			}
+
+			result := SelectExpression{
+				Expr: translatedExpr,
+			}
+
+			result.Column = &Column{
+				MongoType: schema.MongoNone,
+				SQLType:   translatedExpr.Type(),
+				Table:     "", // this needs to come from the alias of the parent...?
+			}
+
+			if typedE.As != nil {
+				result.Name = string(typedE.As)
+			} else if sqlCol, ok := translatedExpr.(SQLColumnExpr); ok {
+				result.Name = sqlCol.columnName
+			}
+
+			// TODO: not sure we need View at all...
+			result.View = result.Name
+
+			results = append(results, result)
+		}
+	}
+
+	return results, nil
+}
+
+func (a *algebrizer) translateTableExprs(tableExprs sqlparser.TableExprs) (PlanStage, error) {
 
 	var plan PlanStage
 	for i, tableExpr := range tableExprs {
-		temp, err := newAlgebrizeTableExpr(tableExpr, ctx)
+		temp, err := a.translateTableExpr(tableExpr)
 		if err != nil {
 			return nil, err
 		}
@@ -74,40 +150,74 @@ func newAlgebrizeTableExprs(tableExprs sqlparser.TableExprs, ctx *AlgebrizerCont
 	return plan, nil
 }
 
-func newAlgebrizeTableExpr(tableExpr sqlparser.TableExpr, ctx *AlgebrizerContext) (PlanStage, error) {
+func (a *algebrizer) translateTableExpr(tableExpr sqlparser.TableExpr) (PlanStage, error) {
 	switch typedT := tableExpr.(type) {
 	case *sqlparser.AliasedTableExpr:
-		return newAlgebrizeSimpleTableExpr(typedT.Expr, string(typedT.As), ctx)
+		return a.translateSimpleTableExpr(typedT.Expr, string(typedT.As))
 	case sqlparser.SimpleTableExpr:
-		return newAlgebrizeSimpleTableExpr(typedT, "", ctx)
+		return a.translateSimpleTableExpr(typedT, "")
 	default:
 		return nil, fmt.Errorf("no support for %T", tableExpr)
 	}
 }
 
-func newAlgebrizeSimpleTableExpr(tableExpr sqlparser.SimpleTableExpr, aliasName string, ctx *AlgebrizerContext) (PlanStage, error) {
+func (a *algebrizer) translateSimpleTableExpr(tableExpr sqlparser.SimpleTableExpr, aliasName string) (PlanStage, error) {
 	switch typedT := tableExpr.(type) {
 	case *sqlparser.TableName:
-		return newAlgebrizeTableName(typedT, aliasName, ctx)
+		tableName := string(typedT.Name)
+		if aliasName == "" {
+			aliasName = tableName
+		}
+
+		dbName := strings.ToLower(string(typedT.Qualifier))
+		if dbName == "" {
+			dbName = a.dbName
+		}
+
+		var plan PlanStage
+		var err error
+		if strings.EqualFold(dbName, InformationDatabase) {
+			plan = NewSchemaDataSourceStage(tableName, aliasName)
+		} else {
+			plan, err = NewMongoSourceStage(a.schema, dbName, tableName, aliasName)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		a.registerColumns(plan.OpFields())
+
+		return plan, nil
 	default:
 		return nil, fmt.Errorf("no support for %T", tableExpr)
 	}
 }
 
-func newAlgebrizeTableName(tableName *sqlparser.TableName, aliasName string, ctx *AlgebrizerContext) (PlanStage, error) {
-	name := string(tableName.Name)
-	if aliasName == "" {
-		aliasName = name
-	}
+func (a *algebrizer) translateExpr(expr sqlparser.Expr) (SQLExpr, error) {
+	switch typedE := expr.(type) {
+	case *sqlparser.ColName:
+		tableName := ""
+		if typedE.Qualifier != nil {
+			tableName = string(typedE.Qualifier)
+		}
 
-	dbName := strings.ToLower(string(tableName.Qualifier))
-	if dbName == "" {
-		dbName = ctx.dbName
-	}
+		columnName := string(typedE.Name)
 
-	if strings.EqualFold(dbName, InformationDatabase) {
-		return NewSchemaDataSourceStage(name, aliasName), nil
-	}
+		column, err := a.lookupColumn(tableName, columnName)
+		if err != nil {
+			return nil, err
+		}
 
-	return NewMongoSourceStage(ctx.schema, dbName, name, aliasName)
+		return SQLColumnExpr{
+			tableName:  column.Table,
+			columnName: column.Name,
+			columnType: schema.ColumnType{
+				SQLType:   column.SQLType,
+				MongoType: column.MongoType,
+			},
+		}, nil
+
+	default:
+		return nil, fmt.Errorf("no support for %T", expr)
+	}
 }
--- evaluator/new_algebrizer_test.go
+++ evaluator/new_algebrizer_test.go
@@ -10,29 +10,81 @@ import (
 
 func TestNewAlgebrize(t *testing.T) {
 
-	schema, _ := schema.New(testSchema1)
+	testSchema, _ := schema.New(testSchema1)
+	defaultDbName := "test"
 
-	test := func(sql, dbName string, expectedPlanFactory func() PlanStage) {
+	test := func(sql string, expectedPlanFactory func() PlanStage) {
 		Convey(sql, func() {
 			statement, err := sqlparser.Parse(sql)
 			So(err, ShouldBeNil)
 
 			selectStatement := statement.(*sqlparser.Select)
-			algebrizerContext := NewAlgebrizerContext("test", schema)
-			actualPlan, err := Algebrize(selectStatement, algebrizerContext)
+			actualPlan, err := Algebrize(selectStatement, defaultDbName, testSchema)
 			So(err, ShouldBeNil)
 
 			expectedPlan := expectedPlanFactory()
+
+			//fmt.Printf("\nExpected: %# v", pretty.Formatter(expectedPlan))
+			//fmt.Printf("\nActual: %# v", pretty.Formatter(actualPlan))
+
 			So(actualPlan, ShouldResemble, expectedPlan)
 		})
 	}
 
+	createSelectExpression := func(tableName, columnName string, expr SQLExpr) SelectExpression {
+		column := &Column{
+			Table:     tableName,
+			Name:      columnName,
+			View:      columnName, // ???
+			SQLType:   expr.Type(),
+			MongoType: schema.MongoNone,
+		}
+
+		return SelectExpression{Column: column, Expr: expr}
+	}
+
 	Convey("Subject: Algebrize", t, func() {
-		test("select a from foo", "test", func() PlanStage {
-			source, _ := NewMongoSourceStage(schema, "test", "foo", "foo")
+		test("select a from foo", func() PlanStage {
+			source, _ := NewMongoSourceStage(testSchema, defaultDbName, "foo", "foo")
 			return &ProjectStage{
 				source: source,
-				sExprs: nil,
+				sExprs: SelectExpressions{
+					createSelectExpression("", "a",
+						SQLColumnExpr{
+							tableName:  "foo",
+							columnName: "a",
+							columnType: schema.ColumnType{
+								SQLType:   schema.SQLInt,
+								MongoType: schema.MongoInt}}),
+				},
+			}
+		})
+
+		test("select foo.a, bar.a from foo, bar", func() PlanStage {
+			fooSource, _ := NewMongoSourceStage(testSchema, defaultDbName, "foo", "foo")
+			barSource, _ := NewMongoSourceStage(testSchema, defaultDbName, "bar", "bar")
+			return &ProjectStage{
+				source: &JoinStage{
+					left:  fooSource,
+					right: barSource,
+					kind:  CrossJoin,
+				},
+				sExprs: SelectExpressions{
+					createSelectExpression("", "a",
+						SQLColumnExpr{
+							tableName:  "foo",
+							columnName: "a",
+							columnType: schema.ColumnType{
+								SQLType:   schema.SQLInt,
+								MongoType: schema.MongoInt}}),
+					createSelectExpression("", "a",
+						SQLColumnExpr{
+							tableName:  "bar",
+							columnName: "a",
+							columnType: schema.ColumnType{
+								SQLType:   schema.SQLInt,
+								MongoType: schema.MongoInt}}),
+				},
 			}
 		})
 	})
