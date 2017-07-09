7cbb0d1ecaa48f26ea3aee480a3e9c1f232bea69
Wisdom Omuya <deafgoat@gmail.com>
2017-07-08 22:31:17 -0400

    BI-1108: fix PPC/Z/Amzn builds

--- testdata/bin/restore-test-data.sh
+++ testdata/bin/restore-test-data.sh
@@ -8,11 +8,24 @@
 
     cd "$PROJECT_DIR"
 
+    test_pipe="$ARTIFACTS_DIR/test_pipe"
+    [ -e $test_pipe ] && rm $test_pipe
+    mkfifo $test_pipe
+    tee -a "$ARTIFACTS_DIR/out/${SUITE}-suite.out" < $test_pipe&
+
+    cd "$PROJECT_DIR"
+
     go test -v \
         -run $^ \
         -timeout 4h \
+        $RACE_DETECTOR \
+        $BUILD_FLAGS \
+        $COVER_FLAG \
         $VERSION_FLAG \
         -restoreData "$SUITE"
+        > $test_pipe
+
+    rm $test_pipe
 
     echo "done restoring $SUITE data"
 
