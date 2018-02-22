8666707c33b0f0eb18df8549c3b7cff188857441
Martin Zhang <Martin.zhang@10gen.com>
2018-02-22 11:21:00 -0500

    BI-521 benchmarks

--- internal/testutils/bench/benchmarks.go
+++ internal/testutils/bench/benchmarks.go
@@ -109,6 +109,7 @@ func restoreBenchmarkData(name string) error {
 }
 
 func runAggBenchmark(b *testing.B, session *mongodb.Session, db, coll string, pipeline []bson.D) {
+	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
 		iter, err := session.Aggregate(db, coll, pipeline)
 		if err != nil {
@@ -125,6 +126,7 @@ func runAggBenchmark(b *testing.B, session *mongodb.Session, db, coll string, pi
 }
 
 func runBenchmark(b *testing.B, db *sql.DB, query string) {
+	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
 		rows, err := db.Query(query)
 		if err != nil {
