3256cfaeb8f382574bd5c4f5b1114a85cf569e2a
Wisdom Omuya <deafgoat@gmail.com>
2018-03-08 14:07:25 -0500

    BI-1515: stop cloning already vendored mysql driver repo

--- testdata/bin/ensure-schema-available.sh
+++ testdata/bin/ensure-schema-available.sh
@@ -4,8 +4,11 @@
 . "$(dirname $0)/prepare-shell.sh"
 
 (
-    echo "getting go mysql driver..."
-    go get github.com/go-sql-driver/mysql
+    MYSQL_VENDOR_PATH=github.com/go-sql-driver/mysql
+    TARGET_DIR=$GOPATH/src/$MYSQL_VENDOR_PATH
+    mkdir -p $TARGET_DIR
+    cp -rf $PROJECT_DIR/vendor/$MYSQL_VENDOR_PATH/* $TARGET_DIR/
+
     echo "ensuring schema availability..."
     go run $(dirname $0)/ensure/schema-available.go
 ) > $LOG_FILE 2>&1
