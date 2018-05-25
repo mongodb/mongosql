538cd48001e403d082ec4a8eb6300c51ec44949f
Ryan Chipman <ryan@ryanchipman.com>
2018-05-25 14:32:47 -0400

    BI-1642: Bump patch versions for tested mongodb releases

--- testdata/bin/download-mongodb.sh
+++ testdata/bin/download-mongodb.sh
@@ -50,9 +50,9 @@ get_mongodb_download_url_for ()
    _DISTRO=$1
    _VERSION=$2
 
-   VERSION_36="3.6.2"
-   VERSION_34="3.4.7"
-   VERSION_32="3.2.16"
+   VERSION_36="3.6.5"
+   VERSION_34="3.4.15"
+   VERSION_32="3.2.20"
 
    EXTRACT="tar zxf"
    # getdata matrix on:
