c2dd427b64ef6c9cce8ba6f96e7338b2492f5f89
Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>
2019-02-27 19:40:05 -0500

    Revert "BI-1930: Update overflow arithmetic to match server behavior (#75)"
    This reverts commit 4dffd3755047f91b694506bd54e90977b79ac3e3.

--- evaluator/expr_binary_op.go
+++ evaluator/expr_binary_op.go
@@ -590,10 +590,6 @@ func (div *SQLDivideExpr) ToAggregationPredicate(t *PushdownTranslator) (interfa
 
 // EvalType returns the EvalType associated with SQLDivideExpr.
 func (div *SQLDivideExpr) EvalType() types.EvalType {
-	// the server returns a decimal if, and only if, either side is decimal.
-	if div.left.EvalType() == types.EvalDecimal128 || div.right.EvalType() == types.EvalDecimal128 {
-		return types.EvalDecimal128
-	}
 	return types.EvalDouble
 }
 
@@ -2255,8 +2251,7 @@ func (sub *SQLSubtractExpr) Evaluate(ctx context.Context, cfg *ExecutionConfig, 
 
 // EvalType returns the EvalType associated with SQLSubtractExpr.
 func (sub *SQLSubtractExpr) EvalType() types.EvalType {
-	// the server determines the result's type based on the input types.
-	return arithmeticEvalType(sub.left, sub.right)
+	return types.EvalDouble
 }
 
 // FoldConstants simplifies expressions containing constants when it is able to for *SQLSubtractExpr.
--- evaluator/expr_evaluate.go
+++ evaluator/expr_evaluate.go
@@ -5,30 +5,7 @@ import (
 
 	"github.com/10gen/sqlproxy/evaluator/types"
 	"github.com/10gen/sqlproxy/evaluator/values"
-
-	"github.com/shopspring/decimal"
 )
-
-// arithmeticEvalType determines the EvalType of the result of
-// an arithmetic operation, given the types of its two operands.
-func arithmeticEvalType(left, right types.EvalTyper) types.EvalType {
-	preferenceType := preferentialType(left, right)
-
-	if left.EvalType() == types.EvalUint64 || right.EvalType() == types.EvalUint64 {
-		preferenceType = types.EvalDecimal128
-	}
-
-	switch preferenceType {
-	case types.EvalDecimal128: // leave as-is
-	case types.EvalDate, types.EvalDatetime, types.EvalInt64, types.EvalInt32:
-		// arithmetic between time types results in an integer
-		preferenceType = types.EvalInt64
-	default:
-		preferenceType = types.EvalDouble
-	}
-
-	return preferenceType
-}
 
 // doArithmetic performs the given arithmetic operation using
 // leftVal and rightVal as operands.
@@ -42,53 +19,54 @@ func doArithmetic(leftVal, rightVal values.SQLValue, op ArithmeticOperator) (val
 	}
 	valueKind := leftVal.Kind()
 
-	preferenceType := arithmeticEvalType(leftVal, rightVal)
+	preferenceType := preferentialType(leftVal, rightVal)
+	useDecimal := preferenceType == types.EvalDecimal128
 
-	// use decimal type if Float64() value loses precision.
-	useDecimal := preferenceType == types.EvalDecimal128 ||
-		values.Int64(leftVal) > maxPrecisionInt ||
-		values.Int64(rightVal) > maxPrecisionInt ||
-		values.Int64(leftVal) < minPrecisionInt ||
-		values.Int64(rightVal) < minPrecisionInt
+	leftType := leftVal.EvalType()
+	rightType := rightVal.EvalType()
 
-	// match server behavior by performing subtraction of int64s using
-	// int64s (this can intentionally lead to overflow).
-	if preferenceType == types.EvalInt64 && op == SUB {
-		return values.NewSQLInt64(valueKind, values.Int64(leftVal)-values.Int64(rightVal)), nil
+	hasUnsigned := leftType == types.EvalUint64 || rightType == types.EvalUint64
+
+	if hasUnsigned {
+		useDecimal = true
+		preferenceType = types.EvalDecimal128
 	}
 
+	// check if both operands are timestamp or date since
+	// arithmetic between time types result in an integer
+	if preferenceType == types.EvalDate || preferenceType == types.EvalDatetime {
+		preferenceType = types.EvalInt64
+	}
+
+	if preferenceType == types.EvalBoolean {
+		preferenceType = types.EvalDouble
+	}
+
+	// use decimal type if Float64() value loses precision
+	useDecimal = useDecimal ||
+		values.Int64(leftVal) > maxPrecisionInt ||
+		values.Int64(rightVal) > maxPrecisionInt
+
 	if useDecimal {
-		var decimalResult decimal.Decimal
 		leftDecimal := values.Decimal(leftVal)
 		rightDecimal := values.Decimal(rightVal)
 		switch op {
 		case ADD:
-			decimalResult = leftDecimal.Add(rightDecimal)
+			return values.NewSQLDecimal128(valueKind, leftDecimal.Add(rightDecimal)), nil
 		case DIV:
-			decimalResult = leftDecimal.Div(rightDecimal)
+			decimalResult := leftDecimal.Div(rightDecimal)
 			// 4 comes from the div_precision_increment variable which
 			// we do not allow to be set.
 			scale := leftDecimal.Exponent() - 4
 			decimalResult = decimalResult.Round(-scale)
 			return values.NewSQLDecimal128(valueKind, decimalResult), nil
 		case MULT:
-			decimalResult = leftDecimal.Mul(rightDecimal)
+			return values.NewSQLDecimal128(valueKind, leftDecimal.Mul(rightDecimal)), nil
 		case SUB:
 			return values.NewSQLDecimal128(valueKind, leftDecimal.Sub(rightDecimal)), nil
 		default:
 			return nil, fmt.Errorf("unrecognized arithmetic operator: %v", op)
 		}
-
-		// perform the operation precisely by using Decimal128s, but
-		// then coerce the result into a float to match the server
-		// behavior. This is necessary since casting the left and
-		// right values to Float64 below would lose precision _before_
-		// the operation; the server loses precision _after_ it.
-		if preferenceType == types.EvalInt64 && (decimalResult.Cmp(minIntAsDecimal) < 0 || decimalResult.Cmp(maxIntAsDecimal) > 0) {
-			f, _ := decimalResult.Float64()
-			return values.NewSQLFloat(valueKind, f), nil
-		}
-		return values.NewSQLDecimal128(valueKind, decimalResult), nil
 	}
 
 	valueF := 0.0
@@ -98,7 +76,8 @@ func doArithmetic(leftVal, rightVal values.SQLValue, op ArithmeticOperator) (val
 	case ADD:
 		valueF = leftFloat + rightFloat
 	case DIV:
-		return values.NewSQLFloat(valueKind, leftFloat/rightFloat), nil
+		floatResult := leftFloat / rightFloat
+		return values.NewSQLFloat(valueKind, floatResult), nil
 	case MULT:
 		valueF = leftFloat * rightFloat
 	case SUB:
@@ -107,9 +86,11 @@ func doArithmetic(leftVal, rightVal values.SQLValue, op ArithmeticOperator) (val
 		return nil, fmt.Errorf("unrecognized arithmetic operator: %v", op)
 	}
 
-	if preferenceType == types.EvalInt64 {
+	switch preferenceType {
+	case types.EvalInt64, types.EvalInt32:
 		return values.NewSQLInt64(valueKind, int64(valueF)), nil
+	case types.EvalDouble:
+		return values.NewSQLFloat(valueKind, valueF), nil
 	}
-
 	return values.NewSQLFloat(valueKind, valueF), nil
 }
