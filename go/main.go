a703b1e3b0976b162c51aa38967f876b483f25c5
Patrick Meredith <pmeredit@gmail.com>
2021-02-10 18:46:36 -0500

    stuff

--- go/main.go
+++ go/main.go
@@ -9,6 +9,6 @@ import (
 func main() {
 	v := mongosql.Version()
 	fmt.Printf("version: %s\n", v)
-	tr := mongosql.Translate("select * from foo join bar")
+	tr := mongosql.Translate("select")
 	fmt.Printf("got translation: %s\n", tr)
 }
