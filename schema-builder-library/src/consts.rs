af57e6f4abf2fd437da2712c16f729e7a83162cf
Nathan Leniz <nathan.leniz@gmail.com>
2024-07-22 16:29:37 -0700

    SQL-2240: schema builder fixes (#769)
    * update try_from impl
    
    * use $set in update operation, proper formatting
    
    * buildinfo fix
    
    * use one concurrency safe structure
    
    * clippy
    
    * cleanup
    
    * ensure __sql_schemas is ignored in analysis
    
    * feedback and fixes
    
    * update name
    
    ---------
    
    Co-authored-by: Nathan Leniz <nathanleniz@mongodb.com>

--- schema-builder-library/src/consts.rs
+++ schema-builder-library/src/consts.rs
@@ -1,10 +1,11 @@
 pub(crate) const DISALLOWED_DB_NAMES: [&str; 4] = ["admin", "config", "local", "system"];
-pub(crate) const DISALLOWED_COLLECTION_NAMES: [&str; 5] = [
+pub(crate) const DISALLOWED_COLLECTION_NAMES: [&str; 6] = [
     "system.namespaces",
     "system.indexes",
     "system.profile",
     "system.js",
     "system.views",
+    "__sql_schemas",
 ];
 
 pub(crate) const PARTITION_SIZE_IN_BYTES: i64 = 100 * 1024 * 1024; // 100 MB
