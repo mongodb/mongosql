537eab2378c4e1ac2a823eafec5f3328c8d0e908
Jonathan Chemburkar <jonathan.chemburkar@mongodb.com>
2025-02-25 12:05:12 -0800

    SQL-2362: implement schema derivation for array ops in $match (#27)
    * add first/last, reverseArray
    
    * add concatArrays
    
    * add indexOfArray
    
    * add array elem at
    
    * add rest of operators and tests
    
    * update intersection to handle additionalProperties
    
    * fix tests
    
    * cleanup
    
    * fix nullish any/allElementsTrue
    
    * more cleanup
    
    * feedback

--- agg-ast/ast/src/serde_test.rs
+++ agg-ast/ast/src/serde_test.rs
@@ -2186,12 +2186,11 @@ mod expression_test {
             definitions::{
                 Accumulator, Bottom, BottomN, Convert, DateAdd, DateDiff, DateExpression,
                 DateFromParts, DateFromString, DateSubtract, DateToParts, DateToString, DateTrunc,
-                Expression, Filter, FirstN, Function, GetField, LastN, Let, Like, LiteralValue,
-                Map, MaxNArrayElement, Median, MinNArrayElement, Percentile, ProjectItem,
-                ProjectStage, Reduce, Ref, RegexAggExpression, Replace, SQLConvert, SQLDivide,
-                SetField, SortArray, SortArraySpec, Stage, Subquery, SubqueryComparison,
-                SubqueryExists, Switch, SwitchCase, TaggedOperator, Top, TopN, Trim, UnsetField,
-                UntaggedOperator, UntaggedOperatorName, Zip,
+                Expression, Filter, Function, GetField, Let, Like, LiteralValue, Map, Median,
+                NArrayOp, Percentile, ProjectItem, ProjectStage, Reduce, Ref, RegexAggExpression,
+                Replace, SQLConvert, SQLDivide, SetField, SortArray, SortArraySpec, Stage,
+                Subquery, SubqueryComparison, SubqueryExists, Switch, SwitchCase, TaggedOperator,
+                Top, TopN, Trim, UnsetField, UntaggedOperator, UntaggedOperatorName, Zip,
             },
             map,
         };
@@ -2815,7 +2814,7 @@ mod expression_test {
         // Array Operators
         test_serde_expr!(
             first_n,
-            expected = Expression::TaggedOperator(TaggedOperator::FirstN(FirstN {
+            expected = Expression::TaggedOperator(TaggedOperator::FirstN(NArrayOp {
                 input: Box::new(Expression::Ref(Ref::FieldRef("a".to_string()))),
                 n: Box::new(Expression::Literal(LiteralValue::Int32(3))),
             })),
@@ -2827,7 +2826,7 @@ mod expression_test {
 
         test_serde_expr!(
             last_n,
-            expected = Expression::TaggedOperator(TaggedOperator::LastN(LastN {
+            expected = Expression::TaggedOperator(TaggedOperator::LastN(NArrayOp {
                 input: Box::new(Expression::Ref(Ref::FieldRef("a".to_string()))),
                 n: Box::new(Expression::Literal(LiteralValue::Int32(3))),
             })),
@@ -2903,11 +2902,10 @@ mod expression_test {
 
         test_serde_expr!(
             max_n_array_element,
-            expected =
-                Expression::TaggedOperator(TaggedOperator::MaxNArrayElement(MaxNArrayElement {
-                    input: Box::new(Expression::Ref(Ref::FieldRef("a".to_string()))),
-                    n: Box::new(Expression::Literal(LiteralValue::Int32(2))),
-                })),
+            expected = Expression::TaggedOperator(TaggedOperator::MaxNArrayElement(NArrayOp {
+                input: Box::new(Expression::Ref(Ref::FieldRef("a".to_string()))),
+                n: Box::new(Expression::Literal(LiteralValue::Int32(2))),
+            })),
             input = r#"expr: {"$maxN": {
                                 "input": "$a",
                                 "n": 2
@@ -2916,11 +2914,10 @@ mod expression_test {
 
         test_serde_expr!(
             min_n_array_element,
-            expected =
-                Expression::TaggedOperator(TaggedOperator::MinNArrayElement(MinNArrayElement {
-                    input: Box::new(Expression::Ref(Ref::FieldRef("a".to_string()))),
-                    n: Box::new(Expression::Literal(LiteralValue::Int32(2))),
-                })),
+            expected = Expression::TaggedOperator(TaggedOperator::MinNArrayElement(NArrayOp {
+                input: Box::new(Expression::Ref(Ref::FieldRef("a".to_string()))),
+                n: Box::new(Expression::Literal(LiteralValue::Int32(2))),
+            })),
             input = r#"expr: {"$minN": {
                                 "input": "$a",
                                 "n": 2
@@ -2977,7 +2974,9 @@ mod expression_test {
         test_serde_expr!(
             zip,
             expected = Expression::TaggedOperator(TaggedOperator::Zip(Zip {
-                inputs: Box::new(Expression::Ref(Ref::FieldRef("a".to_string()))),
+                inputs: Box::new(Expression::Array(vec![Expression::Ref(Ref::FieldRef(
+                    "a".to_string()
+                ))])),
                 use_longest_length: true,
                 defaults: Some(Box::new(Expression::Array(vec![
                     Expression::Literal(LiteralValue::Int32(1)),
@@ -2986,7 +2985,7 @@ mod expression_test {
                 ]))),
             })),
             input = r#"expr: {"$zip": {
-                                "inputs": "$a",
+                                "inputs": ["$a"],
                                 "useLongestLength": true,
                                 "defaults": [1, 2, 3]
             }}"#
@@ -2995,15 +2994,16 @@ mod expression_test {
         test_serde_expr!(
             zip_default_false,
             expected = Expression::TaggedOperator(TaggedOperator::Zip(Zip {
-                inputs: Box::new(Expression::Ref(Ref::FieldRef("a".to_string()))),
+                inputs: Box::new(Expression::Array(vec![Expression::Ref(Ref::FieldRef(
+                    "a".to_string()
+                ))])),
                 use_longest_length: false,
                 defaults: None,
             })),
             input = r#"expr: {"$zip": {
-                                "inputs": "$a",
+                                "inputs": ["$a"],
             }}"#
         );
-
         // date operators
         test_serde_expr!(
             hour_no_timezone,
