f6578369a897d6fca42da14b5f89c8e7438027c7
Nicholas Cioli <nicholascioli@users.noreply.github.com>
2026-04-21 13:52:56 -0400

    include vendored protoc for build script

--- service/build.rs
+++ service/build.rs
@@ -1,9 +1,20 @@
 use std::env;
 use std::path::PathBuf;
 
+use protoc_bin_vendored::protoc_bin_path;
+
 // This generates code from the .proto files located in the service/proto directory
 // using the protoc compiler.
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // We bundle protoc during builds to ensure that the generated output
+    // is always the same and consistent across all build targets
+    let vendored_config = {
+        let mut config = tonic_prost_build::Config::new();
+        config.protoc_executable(protoc_bin_path().expect("valid vendored protoc"));
+
+        config
+    };
+
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let descriptor_path = out_dir.join("translator_descriptor.bin");
     tonic_prost_build::configure()
@@ -12,7 +23,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_server(true)
         .file_descriptor_set_path(&descriptor_path)
         .out_dir(&out_dir)
-        .compile_protos(&["proto/translator/v1/translator.proto"], &["proto/"])?;
+        .compile_with_config(
+            vendored_config,
+            &["proto/translator/v1/translator.proto"],
+            &["proto/"],
+        )?;
 
     Ok(())
 }
