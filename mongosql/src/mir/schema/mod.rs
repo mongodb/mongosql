9c18319c27036b9f6a3932d544e3480538d080e4
Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>
2026-07-07 15:31:32 -0400

    SQL-3294: Update schema checking for varidaic functions to require minimum argument counts

--- mongosql/src/mir/schema/mod.rs
+++ mongosql/src/mir/schema/mod.rs
@@ -29,6 +29,7 @@ pub use errors::Error;
 
 #[cfg(test)]
 pub(crate) use errors::ANY_SCHEMA_ADDENDUM;
+use crate::mir::schema::errors::IncorrectArgCountPrecision;
 
 mod util;
 
@@ -1383,7 +1384,18 @@ trait SqlFunction {
         if found != required {
             return Err(Error::IncorrectArgumentCount {
                 name: self.as_str(),
-                required,
+                required: IncorrectArgCountPrecision::Exact(required),
+                found,
+            });
+        }
+        Ok(())
+    }
+
+    fn ensure_minimum_arg_count(&self, found: usize, required: usize) -> Result<(), Error> {
+        if found < required {
+            return Err(Error::IncorrectArgumentCount {
+                name: self.as_str(),
+                required: IncorrectArgCountPrecision::Minimum(required),
                 found,
             });
         }
@@ -1426,6 +1438,9 @@ trait SqlFunction {
     ///
     /// Since the argument count can vary, the required schema is a single
     /// value that's compared against each of the arguments.
+    ///
+    /// The minimum_args parameter specifies the minimum number of arguments
+    /// that are required for this variadic function.
     fn schema_check_variadic_args(
         &self,
         state: &SchemaInferenceState,
@@ -1517,7 +1532,7 @@ trait SqlFunction {
         let [in_operator_lhs, in_operator_rhs] = arg_schema else {
             return Err(Error::IncorrectArgumentCount {
                 name: self.as_str(),
-                required: 2,
+                required: IncorrectArgCountPrecision::Exact(2),
                 found: arg_schema.len(),
             });
         };
@@ -1713,19 +1728,28 @@ impl ScalarFunction {
         use ScalarFunction::*;
         match self {
             // String operators.
-            Concat => self.propagate_fixed_null_arguments(
-                state,
-                arg_schemas,
-                &[STRING_OR_NULLISH.clone(), STRING_OR_NULLISH.clone()],
-                Schema::Atomic(Atomic::String),
-            ),
+            Concat => {
+                // Concat is a variadic operator, but we require it have at least two arguments.
+                self.ensure_minimum_arg_count(arg_schemas.len(), 2)?;
+                self.propagate_variadic_null_arguments(
+                    state,
+                    arg_schemas,
+                    STRING_OR_NULLISH.clone(),
+                    Schema::Atomic(Atomic::String),
+                )
+            },
             // Unary arithmetic operators.
             Pos | Neg | Abs | Ceil | Floor => {
                 self.ensure_arg_count(arg_schemas.len(), 1)?;
                 self.get_arithmetic_schema(state, arg_schemas)
             }
             // Arithmetic operators with variadic arguments.
-            Add | Mul => self.get_arithmetic_schema(state, arg_schemas),
+            Add | Mul => {
+                // Add and Mul are variadic operators, but we require them to have at least two
+                // arguments.
+                self.ensure_minimum_arg_count(arg_schemas.len(), 2)?;
+                self.get_arithmetic_schema(state, arg_schemas)
+            },
             // Arithmetic operators with fixed (two) arguments.
             Sub | Div => {
                 self.ensure_arg_count(arg_schemas.len(), 2)?;
@@ -1800,12 +1824,17 @@ impl ScalarFunction {
                 from_ref(&BOOLEAN_OR_NULLISH),
                 Schema::Atomic(Atomic::Boolean),
             ),
-            And | Or => self.propagate_variadic_null_arguments(
-                state,
-                arg_schemas,
-                BOOLEAN_OR_NULLISH.clone(),
-                Schema::Atomic(Atomic::Boolean),
-            ),
+            And | Or => {
+                // And and Or are variadic operators, but we require them to have at least two
+                // arguments.
+                self.ensure_minimum_arg_count(arg_schemas.len(), 2)?;
+                self.propagate_variadic_null_arguments(
+                    state,
+                    arg_schemas,
+                    BOOLEAN_OR_NULLISH.clone(),
+                    Schema::Atomic(Atomic::Boolean),
+                )
+            },
             // Computed Field Access operator when the field is not known until runtime.
             ComputedFieldAccess => {
                 self.schema_check_fixed_args(
@@ -1943,7 +1972,7 @@ impl ScalarFunction {
         if arg_schemas.is_empty() {
             return Err(Error::IncorrectArgumentCount {
                 name: self.as_str(),
-                required: 1,
+                required: IncorrectArgCountPrecision::Exact(1),
                 found: 0,
             });
         }
