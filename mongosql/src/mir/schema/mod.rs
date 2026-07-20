f1f09ea0b630517f23c9ccae3e7d2021cf3eec33
Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>
2026-07-20 10:26:04 -0400

    Update mongosql/src/mir/schema/mod.rs
    Co-authored-by: Jonathan Chemburkar <jchemburkar87@gmail.com>

--- mongosql/src/mir/schema/mod.rs
+++ mongosql/src/mir/schema/mod.rs
@@ -2198,7 +2198,7 @@ trait HigherOrderFunction: SqlFunction {
     /// `HigherOrderFunctionErrorCause` type for the full set of possible causes.
     ///
     /// Callers of this method should provide the specific `value_cause` that best describes the
-    /// cause of the error. The two options are `InvalidInitialValue` and `InvalidAccumulatedValue`.
+    /// cause of the error. The two options are `InitialValueUsage` and `AccumulatedValueUsage`.
     /// For example, if the error occurred because the `value` variable was schema-checked with the
     /// initial value schema, the `value_cause` should be `InvalidInitialValue`. Otherwise it should
     /// be `InvalidAccumulatedValue`.
